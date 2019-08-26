dc9da7697785ade71c681292c8f1064de93a09a0
Serge <ua9msn@gmail.com>
2019-08-26 16:44:54 +0300

    String literal

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,12 +11,39 @@ describe('Compiler', () => {
             {script: '1 > 2;', argument: {}, result: false, expect: ' = false '},
             {script: '1 < 2;', argument: {}, result: true, expect: ' = true '},
             {script: '1 == 1;', argument: {a: 10}, result: true, expect: ' = true '},
+        ];
+        tests
+            .forEach(test => {
+                const name = test.script + ' ' + (test.expect || '');
+                it(name, function () {
+                    const expression = compile(test.script);
+                    const result = expression(test.argument);
+                    assert.equal(result, test.result, '= ');
+                })
+            })
+    });
+    describe('{a: 10}', () => {
+        const tests = [
             {script: 'record.a > 0;', argument: {a: 10}, result: true, expect: ' = true '},
             {script: 'record.a < 0;', argument: {a: 10}, result: false, expect: ' = false '},
             {script: 'record.a && record.a.b && record.a.b.c == null;', argument: {a: 10}, result: null, expect: ' true '},
-            {script: 'record.a == "a";', argument: {a: 'a'}, result: true, expect: ' = true '},
-            {script: 'record.a;', argument: {a: 'a'}, result: 'a', expect: 'property accessor ok'},
             {script: 'record.a * 10;', argument: {a: 10}, result: 100, expect: ' = 100 '},
+        ];
+        tests
+            .forEach(test => {
+                const name = test.script + ' ' + (test.expect || '');
+                it(name, function () {
+                    const expression = compile(test.script);
+                    const result = expression(test.argument);
+                    assert.equal(result, test.result, '= ');
+                })
+            })
+    });
+    describe('{a: "a"}', () => {
+        const tests = [
+            {script: 'record.a == "a";', argument: {a: 'a'}, result: true, expect: ' = true '},
+            {script: 'record.a == "b";', argument: {a: 'a'}, result: false, expect: ' = false '},
+            {script: 'record.a;', argument: {a: 'a'}, result: 'a', expect: 'property accessor ok'},
         ];
         tests
             .forEach(test => {
@@ -43,4 +70,22 @@ describe('Compiler', () => {
                 })
             })
     });
+
+    describe(' boolean & null ', () => {
+        const tests = [
+            {script: 'record.a == undefined;', argument: {b: 10}, result: true, expect: '{b: 10} true'},
+            {script: 'record.a == true;', argument: {a: true}, result: true, expect: '{a: true}'},
+            {script: 'record.a == false;', argument: {a: false}, result: true, expect: '{a: false}'},
+        ];
+        tests
+            .forEach(test => {
+                const name = test.script + ' ' + (test.expect || '');
+                it(name, function () {
+                    const expression = compile(test.script);
+                    const result = expression(test.argument);
+                    assert.equal(result, test.result);
+                })
+            })
+    });
+
 });
